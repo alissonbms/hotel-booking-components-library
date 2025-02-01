a4f68c32ba6a89f64ee2072ecd9ee7c3e867f3cd
alissonbms <alissonnewdev@gmail.com>
2025-02-01 12:29:55 -0300

    feat: create and export Chip component

---
+++ src/components/Chip/Chip.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './Chip.module.scss';
+
+export interface ChipProps {
+  title: string;
+  width: number;
+  color?: string;
+  textColor?: string;
+  type?: 'filled' | 'outlined';
+}
+
+const Chip = ({ title, width, color, textColor, type }: ChipProps) => {
+  const style =
+    type === 'filled'
+      ? {
+          backgroundColor: color,
+          color: textColor,
+        }
+      : { color: color, border: `1px solid ${color}` };
+
+  return (
+    <div className={styles.chip} style={{ ...style, width: `${width}px` }}>
+      {title}
+    </div>
+  );
+};
+
+export default Chip;
