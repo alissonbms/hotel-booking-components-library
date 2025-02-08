202778cac9ca8942df4433236130d7066b5d461a
alissonbms <alissonnewdev@gmail.com>
2025-02-08 00:50:28 -0300

    feat: create story to Button component

---
+++ src/stories/Button.stories.ts
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Button from '../components/Button/Button';
+
+const meta: Meta<typeof Button> = {
+  title: 'Components/Button',
+  component: Button,
+  parameters: {  
+    layout: 'centered',
+  },
+  argTypes: {}, 
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Rounded: Story = {
+  args: {
+    title: 'Rounded button',
+    children: 'Rounded button',
+    variant: 'rounded',
+    colors: 'orange',
+    width: 300,
+  },
+};
+
+export const Normal: Story = {
+  args: {
+    title: 'Normal button',
+    children: 'Normal button',
+    variant: 'normal',
+    colors: 'orange__outline',
+    width: 300,
+  },
+};
+
+export const White: Story = {
+  args: {
+    title: 'White button',
+    children: 'White button',
+    variant: 'rounded',
+    colors: 'white',
+    width: 300,
+  },
+};
